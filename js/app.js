c8341bee45e6f2c272b6adfc6a9b0ccc8a27bff2
Michele Saba <michelemsaba@gmail.com>
2019-03-29 09:12:08 -0500

    added images tto home screen

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@
 
 // TODO: Create a "Cart" constructor (named Cart) that has item and quantity
 // properties, and builds an array of items as you create instances
-var Cart = function(item, quantity) {
+var Cart = function(item, quantity, filepath) {
   this.item = item; 
   this.quantity = quantity; 
+  this.filePath = filePath;
   Cart.allItems.push(this);
 }
 Cart.allItems = [];
@@ -20,26 +21,39 @@ var Product = function(filePath, name) {
 Product.allProducts = [];
 
 function generateCatalog() {
-  new Product('assets/bag.jpg', 'Bag');
-  new Product('assets/banana.jpg', 'Banana');
-  new Product('assets/bathroom.jpg', 'Bathroom');
-  new Product('assets/boots.jpg', 'Boots');
-  new Product('assets/breakfast.jpg', 'Breakfast');
-  new Product('assets/bubblegum.jpg', 'Bubblegum');
-  new Product('assets/chair.jpg', 'Chair');
-  new Product('assets/cthulhu.jpg', 'Cthulhu');
-  new Product('assets/dog-duck.jpg', 'Dog-Duck');
-  new Product('assets/dragon.jpg', 'Dragon');
-  new Product('assets/pen.jpg', 'Pen');
-  new Product('assets/pet-sweep.jpg', 'Pet Sweep');
-  new Product('assets/scissors.jpg', 'Scissors');
-  new Product('assets/shark.jpg', 'Shark');
-  new Product('assets/sweep.png', 'Sweep');
-  new Product('assets/tauntaun.jpg', 'Taun-Taun');
-  new Product('assets/unicorn.jpg', 'Unicorn');
-  new Product('assets/usb.gif', 'USB');
-  new Product('assets/water-can.jpg', 'Water Can');
-  new Product('assets/wine-glass.jpg', 'Wine Glass');
+  new Product('./img/bag.jpg', 'Bag');
+  new Product('./img/banana.jpg', 'Banana');
+  new Product('./img/bathroom.jpg', 'Bathroom');
+  new Product('./img/boots.jpg', 'Boots');
+  new Product('./img/breakfast.jpg', 'Breakfast');
+  new Product('./img/bubblegum.jpg', 'Bubblegum');
+  new Product('./img/chair.jpg', 'Chair');
+  new Product('./img/cthulhu.jpg', 'Cthulhu');
+  new Product('./img/dog-duck.jpg', 'Dog-Duck');
+  new Product('./img/dragon.jpg', 'Dragon');
+  new Product('./img/pen.jpg', 'Pen');
+  new Product('./img/sweep.jpg', 'Pet Sweep');
+  new Product('./img/scissors.jpg', 'Scissors');
+  new Product('./img/shark.jpg', 'Shark');
+  new Product('./img/sweep.jpg', 'Sweep');
+  new Product('./img/tauntaun.jpg', 'Taun-Taun');
+  new Product('./img/unicorn.jpg', 'Unicorn');
+  new Product('./img/usb.gif', 'USB');
+  new Product('./img/water-can.jpg', 'Water Can');
+  new Product('./img/wine-glass.jpg', 'Wine Glass');
+}
+
+function addImgCat() {
+  var selectElement = document.getElementById('catalog');
+  for (var i in Product.allProducts) {
+    var img = document.createElement('img');
+    img.textContent = Product.allProducts[i].filePath;
+    img.setAttribute('data-index', Product.allProducts[i]);
+    img.src = Product.allProducts[i].filePath;
+      catalog.appendChild(img);
+  }
 }
 // Initialize the app by creating the big list of products with images and names
+
 generateCatalog();
+addImgCat();
